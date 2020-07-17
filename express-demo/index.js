6c307f0525b6daa5f0dcfc875e07e6b328c661e6
Kelly online <kellyonline2016@gmail.com>
2020-07-17 23:05:46 +0100

    added a new route that sends an array

--- express-demo/index.js
+++ express-demo/index.js
@@ -6,5 +6,9 @@ app.get('/', (request, response) => {
     response.send('Hello world');
 } )
 
+app.get('/api/courses', (request, response) => {
+    response.send([1, 2, 3])
+})
+
 app.listen(3000, () => {console.log('Server is running on port 3000');
 })
