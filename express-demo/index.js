12951a999cfd33dc3d0a2393b0ba1b9f472e1960
Kelly online <kellyonline2016@gmail.com>
2020-07-17 23:26:49 +0100

    SET environment variable

--- express-demo/index.js
+++ express-demo/index.js
@@ -11,7 +11,7 @@ app.get('/api/courses', (request, response) => {
 })
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {console.log(`Server is running on port ${port}`);
 })
