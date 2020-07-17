cf165e31e988f47a47caf07f2f24ba3c01f83768
Kelly online <kellyonline2016@gmail.com>
2020-07-17 23:55:20 +0100

    using multiple route parameters

--- express-demo/index.js
+++ express-demo/index.js
@@ -17,6 +17,18 @@ app.get('/api/courses/:id', (request, response) => {
     response.send(request.params)
 })
 
+// get courses by 2 id parameter
+app.get('/api/courses/:id/:name', (request, response) => {
+    response.send(request.params.id)  
+
+    // response.send(request.params.name)
+})
+
+// get courses by 2 id parameter
+app.get('/api/courses/:year/:month', (request, response) => {
+    response.send(request.params)
+})
+
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {console.log(`Server is running on port ${port}`);
