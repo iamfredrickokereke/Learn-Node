06227841fe21f84197fe4c8cc8db3dcc157759a7
Kelly online <kellyonline2016@gmail.com>
2020-07-19 10:35:41 +0100

    create list of genres and retreive all

--- vidly/index.js
+++ vidly/index.js
@@ -2,10 +2,19 @@ const express = require('express');
 
 const app = express();
 
+const genres = [
+    {"id": 1, "type": "Animation"},
+    {"id": 2, "type": "Action"},
+    {"id": 3, "type": "Comedy"},
+    {"id": 4, "type": "Crime"},
+]
+
 // default route
 app.get('/', (request, response) => { response.send('Welcome to Vidly Rental service!!!')})
 
-// app.get()
+app.get('/api/genres', (request, response) => {
+    response.send(genres)
+})
 
 // app.post()
 
